ccb60f6bcd674e7f292929a2bfd411df263055f5
Rakesh <rakesh@zigme.in>
2026-03-12 12:45:23 +0530

    feat: setup shopping cart page and initialize state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let { data } = $props();
+
+	let cart = $derived(data.cart);
+</script>
+
+<h1>Shopping Cart</h1>
